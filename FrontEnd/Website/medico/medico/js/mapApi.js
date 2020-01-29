e27e541cba3ac78c534185edfdfd63b4f9e3fec4
NIghtwarri0R <gamena750@abv.bg>
2020-01-29 15:31:42 +0000

    AJax call in progress
    Need to  test it on Alek's pc

--- FrontEnd/Website/medico/medico/js/mapApi.js
+++ FrontEnd/Website/medico/medico/js/mapApi.js
@@ -1,5 +1,6 @@
 
 
+
 var curr_location = document.getElementById('curr_location');
 
 function get_location(){
@@ -8,11 +9,39 @@ function get_location(){
     navigator.geolocation.getCurrentPosition(position => {
         console.log(position.coords);
         print(position,coords);
+        
     });
     } else {
         console.log("Error occured");
-        console.log("Hi there")
+        
 }
 
 
 }
+function zip_code_info(){
+    var zip_code = document.getElementById('Submit');
+    var zip = document.getElementsByName("zip_code").values(); 
+    console.log(zip);
+    $.ajax({
+        url: 'https://geocoder.ls.hereapi.com/6.2/geocode.json',
+        type: 'GET',
+        dataType: 'jsonp',
+        jsonp: 'jsoncallback',
+        data: {
+        PostalCode: zip,
+        country: 'usa',
+        gen: '9',
+        apiKey: 'wS3zBaE7wLZ5Im9u7TfCFwbDmPABPKlSCCg_7s4JM-U'
+        },
+        success: function (data) {
+        alert(JSON.stringify(data));
+        }
+    });
+
+}
+
+
+function submision(){
+    zip_code_info();
+}
+
