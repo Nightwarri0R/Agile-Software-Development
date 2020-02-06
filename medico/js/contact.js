03605e8933b4052bbb4ff0a26e84d244e27a2fc8
NIghtwarri0R <gamena750@abv.bg>
2020-02-06 13:56:28 +0000

    Fixed an issue with contact js
    There was an issue, but its fixed now

--- medico/js/contact.js
+++ medico/js/contact.js
@@ -1,13 +1,3 @@
 function mail_pop_up(){
-<<<<<<< HEAD
 alert("Message sent")
-
-=======
-    var left = (screen.width/2)-(w/2);
-    var top = (screen.height/2)-(h/2);
-    return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-
-
-
->>>>>>> update-contacts-page
 }
