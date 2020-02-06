be1738a6f52a8418380db53b24ce48ad1f31603c
NIghtwarri0R <gamena750@abv.bg>
2020-02-06 13:51:51 +0000

    Pop done needs a bit of customising

--- medico/js/contact.js
+++ medico/js/contact.js
@@ -1,8 +1,4 @@
 function mail_pop_up(){
-    var left = (screen.width/2)-(w/2);
-    var top = (screen.height/2)-(h/2);
-    return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-
-
+alert("Message sent")
 
 }
